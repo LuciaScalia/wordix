60272d1a2f57a0719265c416a7b2ed132704aa16
LuciaScalia <luciaxscaliax@gmail.com>
2022-11-25 15:02:32 -0300

    Explicación 3, punto 7

--- programaScaliaCabezasJiménezBucareyGonzalez.php
+++ programaScaliaCabezasJiménezBucareyGonzalez.php
@@ -67,21 +67,21 @@ function seleccionarOpcion()
    //int $opcionElegida
 
    echo "
-    1) Jugar al Wordix con una palabra elegida 
-    2) Jugar al Wordix con una palabra aleatoria
-    3) Mostrar una partida
-    4) Mostrar la primer partida ganadora
-    5) Mostrar resumen de Jugador
-    6) Mostrar listado de partidas ordenadas por jugador y por palabra
-    7) Agregar una palabra de 5 letras a Wordix
-    8) Salir
+1) Jugar al Wordix con una palabra elegida 
+2) Jugar al Wordix con una palabra aleatoria
+3) Mostrar una partida
+4) Mostrar la primer partida ganadora
+5) Mostrar resumen de Jugador
+6) Mostrar listado de partidas ordenadas por jugador y por palabra
+7) Agregar una palabra de 5 letras a Wordix
+8) Salir
 
 Elija la opción: ";
 
    $opcionElegida = trim(fgets(STDIN));
 
    while ($opcionElegida < 1 || $opcionElegida > 8) {
-       echo "\nLa opción elegida no es válida, vuelva a ingresarla: ";
+       echo "\n La opción elegida no es válida, vuelva a ingresarla: ";
        $opcionElegida = trim(fgets(STDIN));
    }
 
@@ -103,6 +103,8 @@ Elija la opción: ";
 
     return $coleccionPalabras;
   }
+
+  seleccionarOpcion();
 
 /**************************************/
 /*********** PROGRAMA PRINCIPAL *******/
